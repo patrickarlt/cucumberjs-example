91448b384791458e7ebbf494f4c38763c2bfb4dc
Patrick Arlt <patrick.arlt@gmail.com>
2013-12-11 08:32:57 -0800

    update wd.js, better world setup

--- features/support/world.js
+++ features/support/world.js
@@ -1,20 +1,22 @@
 var wd = require('wd');
 
+var remote = wd.remote(); // this.browser will be available in step definitions
+
+// log status output from web driver
+remote.on('status', function(info){
+  console.log('\x1b[36m%s\x1b[0m', info);
+});
+
+// log commands from web driver
+remote.on('command', function(meth, path, data){
+  console.log(' > \x1b[33m%s\x1b[0m: %s', meth, path, data || '');
+});
+
 var World = function World(callback) {
-  this.browser = wd.remote(); // this.browser will be available in step definitions
-
-  // log status output from web driver
-  this.browser.on('status', function(info){
-    console.log('\x1b[36m%s\x1b[0m', info);
-  });
-
-  // log commands from web driver
-  this.browser.on('command', function(meth, path, data){
-    console.log(' > \x1b[33m%s\x1b[0m: %s', meth, path, data || '');
-  });
+  this.browser = remote;
 
   // run the callback when we are done do cucumber knows we are ready
-  this.browser.init(function() {
+  this.browser.init({browserName: 'firefox'}, function() {
     callback();
   });
 };
